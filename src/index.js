0804f9f640ff53b587196a4a2b99a2f7c385ac1e
Daniel Antelo <daniel.antelo@worldfirst.com>
2017-03-08 12:23:41 +0000

    Fix comment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ function Module() {
   this.name = 'world';
 }
 
-Module.prototype.sayHello = function(){
+Module.prototype.sayHello = function() {
   return 'hello ' + this.name;
 };
 
-// export as module for the node environment (needed for tests to pick your js up)
+// export as module for the node environment (needed for tests to import the js)
 if (typeof exports !== 'undefined') {
   exports.Module = Module
 }
